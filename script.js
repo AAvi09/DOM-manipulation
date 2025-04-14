b4cf13c1c2c4f2a5865495e1e66774c55c4f1d91
Avinash sharma <sharmaavi2196@gmail.com>
2025-04-15 00:33:55 +0530

    Enhance friend button functionality and update card styles

--- script.js
+++ script.js
@@ -4,21 +4,25 @@ const addFriend = document.querySelector("#add");
 
 const removeFriend = document.querySelector("#remove");
 
+const card = document.querySelector("#card");
+
 let char = 0;
 
 addFriend.addEventListener("click", () => {
   if (char == 0) {
     char = 1;
     istatus.innerHTML = "Friends";
-    istatus.style.color = "green";
+    istatus.style.color = "white";
     addFriend.style.background = "linear-gradient(to right, #00c6ff, #0072ff";
     addFriend.innerHTML = "Remove friend";
-  } else {
+    card.style.background = "linear-gradient(to right, #00c6ff, #0072ff";
+  } else if (char == 1) {
     char = 0;
     istatus.innerHTML = "strangers";
-    istatus.style.color = "red";
+    istatus.style.color = "black";
     addFriend.style.background = "linear-gradient(to right, #ff512f, #dd2476";
     addFriend.innerHTML = "Add friend";
+    card.style.background = "linear-gradient(to right, #ff512f, #dd2476";
   }
 });
 
