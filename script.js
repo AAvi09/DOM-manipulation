60fe10d34f0b2dbf7a02017c1c7d6319842ab176
Avinash sharma <sharmaavi2196@gmail.com>
2025-04-15 00:22:24 +0530

    Toggle friend button functionality and update styles

--- script.js
+++ script.js
@@ -11,10 +11,14 @@ addFriend.addEventListener("click", () => {
     char = 1;
     istatus.innerHTML = "Friends";
     istatus.style.color = "green";
+    addFriend.style.background = "linear-gradient(to right, #00c6ff, #0072ff";
+    addFriend.innerHTML = "Remove friend";
   } else {
     char = 0;
     istatus.innerHTML = "strangers";
     istatus.style.color = "red";
+    addFriend.style.background = "linear-gradient(to right, #ff512f, #dd2476";
+    addFriend.innerHTML = "Add friend";
   }
 });
 
