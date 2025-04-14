49011accf0044f55dc8e980b1083851fad69b3a9
Avinash sharma <sharmaavi2196@gmail.com>
2025-04-15 00:00:05 +0530

    Toggle friend status display on button click

--- script.js
+++ script.js
@@ -4,12 +4,21 @@ const addFriend = document.querySelector("#add");
 
 const removeFriend = document.querySelector("#remove");
 
+let char = 0;
+
 addFriend.addEventListener("click", () => {
-  istatus.innerHTML = "Friends";
-  istatus.style.color = "green";
+  if (char == 0) {
+    char = 1;
+    istatus.innerHTML = "Friends";
+    istatus.style.color = "green";
+  } else {
+    char = 0;
+    istatus.innerHTML = "strangers";
+    istatus.style.color = "red";
+  }
 });
 
-removeFriend.addEventListener("click", () => {
-  istatus.innerHTML = "strangers";
-  istatus.style.color = "red";
-});
+// removeFriend.addEventListener("click", () => {
+//   istatus.innerHTML = "strangers";
+//   istatus.style.color = "red";
+// });
